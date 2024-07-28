218e2edb240d2a7c01d74cc33c9eeeef98d58e91
Ricardo Pardini <ricardo@pardini.net>
2024-07-29 00:27:41 +0200

    add test getting a list of SW Films that's not empty
    - this java codegen machinery is pretty useless, if the projection determines the final time
      - neither DGS nor Spring/Java GraphQL handle it, as far as I understand
        - I _think_ the StrawberryShake codegen handles projections better than this

--- src/test/java/net/k8savengers/vaadingraphql/VaadingraphqlApplicationTests.java
+++ src/test/java/net/k8savengers/vaadingraphql/VaadingraphqlApplicationTests.java
@@ -1,13 +1,31 @@
 package net.k8savengers.vaadingraphql;
 
+import net.k8savengers.vaadingraphql.codegen.client.AllFilmsGraphQLQuery;
+import net.k8savengers.vaadingraphql.codegen.client.AllFilmsProjectionRoot;
+import net.k8savengers.vaadingraphql.codegen.types.Film;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.graphql.client.DgsGraphQlClient;
 
 @SpringBootTest
 class VaadingraphqlApplicationTests {
 
+    @Autowired
+    private DgsGraphQlClient dgsClient;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testGraphQLClient() {
+        var list = dgsClient.request(new AllFilmsGraphQLQuery.Builder().build())
+                .projection(new AllFilmsProjectionRoot<>().films().title().director().id())
+                .retrieveSync("allFilms.films").toEntityList(Film.class);
+
+        assert !list.isEmpty();
+
     }
 
 }
